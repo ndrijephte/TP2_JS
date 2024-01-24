34d641bd908c7b5e6ba4038721e7bbede47a4188
ndrijephte <ndrijephtekoffi@gmail.com>
2024-01-24 13:40:38 +0000

    fin du tp2 JS

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Créer un événément au scroll
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  if (window.scrollY < lastScroll) {
+    navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-60px";
+  }
+
+  lastScroll = window.scrollY;
+});
 
 // Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
 
